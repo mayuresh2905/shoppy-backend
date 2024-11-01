e4c6454d73ecdcca152fc4f69a876ae816a7c437
Mayuresh <mayureshprabhu29@gmail.com>
2024-11-01 18:54:33 +0530

    Stripe Fixed

--- src/controllers/payment.ts
+++ src/controllers/payment.ts
@@ -1,30 +1,82 @@
 import { stripe } from "../app.js";
 import { TryCatch } from "../middleware/error.js";
 import { Coupon } from "../models/coupon.js";
+import { Product } from "../models/product.js";
+import { User } from "../models/user.js";
+import { OrderItemType, ShippingInfoType } from "../types/types.js";
 import ErrorHandler from "../utils/utility-class.js";
 
 
 
-export const createPaymentIntent = TryCatch(async (req,res,next) => {
+export const createPaymentIntent = TryCatch(async (req, res, next) => {
+  const { id } = req.query;
 
-  const { amount} = req.body;
+  const user = await User.findById(id).select("name");
 
-  if(!amount) return next(new ErrorHandler("Please enter  amount",400 ));
+  if (!user) return next(new ErrorHandler("Please login first", 401));
+
+  const {
+    items,
+    shippingInfo,
+    coupon,
+  }: {
+    items: OrderItemType[];
+    shippingInfo: ShippingInfoType | undefined;
+    coupon: string | undefined;
+  } = req.body;
+
+  if (!items) return next(new ErrorHandler("Please send items", 400));
+
+  if (!shippingInfo)
+    return next(new ErrorHandler("Please send shipping info", 400));
+
+  let discountAmount = 0;
+
+  if (coupon) {
+    const discount = await Coupon.findOne({ code: coupon });
+    if (!discount) return next(new ErrorHandler("Invalid Coupon Code", 400));
+    discountAmount = discount.amount;
+  }
+
+  const productIDs = items.map((item) => item.productId);
+
+  const products = await Product.find({
+    _id: { $in: productIDs },
+  });
+
+  const subtotal = products.reduce((prev, curr) => {
+    const item = items.find((i) => i.productId === curr._id.toString());
+    if (!item) return prev;
+    return curr.price * item.quantity + prev;
+  }, 0);
+
+  const tax = subtotal * 0.18;
+
+  const shipping = subtotal > 1000 ? 0 : 200;
+
+  const total = Math.floor(subtotal + tax + shipping - discountAmount);
 
   const paymentIntent = await stripe.paymentIntents.create({
-    amount: Number(amount) * 100,
+    amount: total * 100,
     currency: "inr",
-  })
-
-  
+    description: "Shoppy-Payment-Details",
+    shipping: {
+      name: user.name,
+      address: {
+        line1: shippingInfo.address,
+        postal_code: shippingInfo.pinCode.toString(),
+        city: shippingInfo.city,
+        state: shippingInfo.state,
+        country: shippingInfo.country,
+      },
+    },
+  });
 
   return res.status(201).json({
-      success:true,
-      clientSecret: paymentIntent.client_secret
-  })
+    success: true,
+    clientSecret: paymentIntent.client_secret,
+  });
 });
-
-
 
 export const newCoupon = TryCatch(async (req,res,next) => {
 
